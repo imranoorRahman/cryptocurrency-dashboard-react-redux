7e5f3089095267280805d34672f214be2495bd19
Imran Rahman <mrimranoor@gmail.com>
2021-11-22 19:07:41 +0000

    Redux toolkit, API dev, correctly accessing .env variables & linked crypto and news page on the homepage

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,15 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
-    'x-rapidapi-host': process.env.RAPIDAPI_HOST,
-    'x-rapidapi-key': process.env.RAPIDAPI_KEY
+    'x-rapidapi-host': process.env.REACT_APP_RAPIDAPI_HOST,
+    'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY
 };
 
 const createRequest = (url) => ({url, headers: cryptoApiHeaders});
 
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({baseUrl: process.env.CRYPTO_API_URL}),
+    baseQuery: fetchBaseQuery({baseUrl: process.env.REACT_APP_CRYPTO_API_URL}),
     endpoints: (builder) => ({
         getCryptos: builder.query({
             query: () => createRequest('/coins')
