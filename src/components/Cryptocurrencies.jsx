28cafa474ee4615c7565ccaaf7fa19a97aefb578
Imran Rahman <mrimranoor@gmail.com>
2021-11-22 20:14:28 +0000

    fixed gutter issue & added price, marketcap and price change in each card

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import minnify from "millify";
+import minnify, { millify } from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
 import { useGetCryptosQuery } from "../services/cryptoApi";
@@ -10,14 +10,19 @@ const Cryptocurrencies = () => {
   console.log(cryptos);
   return (
     <>
-      <Row gutters={[32, 32]} className="crypto-card-container">
+      <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos.map((crypto) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={crypto.id}>
             <Link to={`/crypto/${crypto.id}`}>
               <Card
                 title={`${crypto.rank} ${crypto.name}`}
                 extra={<img className="crypto-image" src={crypto.iconUrl} />}
-              ></Card>
+                hoverable
+              >
+                <p>Price: {millify(crypto.price)}</p>
+                <p>Market Cap: {millify(crypto.marketCap)}</p>
+                <p>Daily Change: {millify(crypto.change)}</p>
+              </Card>
             </Link>
           </Col>
         ))}
