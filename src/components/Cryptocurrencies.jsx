8011a82246a10f4fa58551198506ba9ff5197620
Imran Rahman <mrimranoor@gmail.com>
2021-11-22 19:34:22 +0000

    loop over top 50 coins & render its name and image in order

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import minnify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const Cryptocurrencies = () => {
-  return <div>Cryptocurrencies</div>;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  console.log(cryptos);
+  return (
+    <>
+      <Row gutters={[32, 32]} className="crypto-card-container">
+        {cryptos.map((crypto) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={crypto.id}>
+            <Link to={`/crypto/${crypto.id}`}>
+              <Card
+                title={`${crypto.rank} ${crypto.name}`}
+                extra={<img className="crypto-image" src={crypto.iconUrl} />}
+              ></Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Cryptocurrencies;
