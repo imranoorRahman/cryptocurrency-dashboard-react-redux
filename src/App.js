947e941d8c41b688e9761fccab7900988d0d5382
Imran Rahman <mrimranoor@gmail.com>
2021-11-22 11:55:18 +0000

    added navbar with logo & styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Router, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
+import {Navbar} from './components';
+import './App.css';
 
 const App = () => {
     return (
         <div className="app">
            <div className="navbar">
-
+                <Navbar />
            </div>
            <div className="main">
 
