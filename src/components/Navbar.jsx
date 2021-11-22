6d6adc61d5330e20712cce571bc41e83648f7b53
Imran Rahman <mrimranoor@gmail.com>
2021-11-22 12:02:53 +0000

    populated navbar div

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,6 +21,20 @@ const Navbar = () => {
         </Typography.Title>
         {/* <button className="menu-control-container"></button> */}
       </div>
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+      </Menu>
     </div>
   );
 };
